3090b338565e9a4721b8b0ad9a95ccf2f7c1167f
sheenugh <delimasheenamae16@gmail.com>
2024-02-20 01:19:52 +0800

    done

--- Desktop/[Practice] Programming/[PRACTICE] Random/DCC/Day 9/counting_lines_in_a_file.py
+++ Desktop/[Practice] Programming/[PRACTICE] Random/DCC/Day 9/counting_lines_in_a_file.py
@@ -1,4 +1,15 @@
 # ========== PSEUDO CODE ==========
 # - Def function
-# - Using for loop
+def count_lines():
+    file = open("story.txt","r")
+    count= 0
+    
+    # - Using for loop
+    for i in file:
+        count+= 1
+    file.close()
+    print("The number of lines in the 'story.txt' file is", count)
+
 # - Caller of the def function
+count_lines()
+
