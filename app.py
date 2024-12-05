d28d2c2d337e80a4d14676a05c8c820acc210240
ProgrammerAngelo <angelovpangilagan@gmail.com>
2024-12-05 04:49:01 -0500

    Base web

--- app.py
+++ app.py
@@ -1,22 +1,30 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
 
 @app.route('/profile')
 def profile():
     return render_template('profile.html')
 
-@app.route('/works', methods=['GET', 'POST'])
+@app.route('/works')
 def works():
-    result = None
-    if request.method == 'POST':
-        input_string = request.form.get('inputString', '')
-        result = input_string.upper()
-    return render_template('touppercase.html', result=result)
+    return render_template('works.html')
+
+@app.route('/link_list')
+def link_list():
+    return render_template('link_list.html')
+
+@app.route('/stack')
+def stack():
+    return render_template('stack.html')
+
+@app.route('/queue')
+def queue():
+    return render_template('queue.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
